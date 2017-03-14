4a522682054ebef945b8f5a8ee981980d2c874d2
Zareena Sofia <KH2116@kitspl.com>
2017-03-14 13:33:56 +0530

    initial commit

--- visualizer application/Foodie/controllers/mobile/FlexContainer0a038f14ef33446Controller/FlexContainer0a038f14ef33446ControllerActions.js
+++
@@ -1,9 +0,0 @@
-define({
-    /*
-      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
-    */
-    AS_FlexContainer_2661e555919d430cb950404c7f89eca6: function AS_FlexContainer_2661e555919d430cb950404c7f89eca6(eventobject) {
-        var self = this;
-        this.testMethod();
-    }
-});
--- visualizer application/Foodie/studioactions/common/AS_FlexContainer_e88fd6abbf1b49cdad910feff23d7328.js
+++
@@ -1,4 +0,0 @@
-function AS_FlexContainer_e88fd6abbf1b49cdad910feff23d7328(eventobject) {
-    var self = this;
-    return;
-}
